f78135984309d75c4d7fab6cb1b7a4b3a18c97e9
amyxlase <amyclin9@gmail.com>
2023-02-11 22:12:52 -0500

    commented scripts & reset scenes

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -12,7 +12,9 @@ public class Gem : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
+	// Let's rotate the trasform of each gem here using the rotation 
+	// angle, rotation speed, and the delta time between updates !
 	void Update () {
-        transform.Rotate(rotationAngle * rotationSpeed * Time.deltaTime);
+        
 	}
 }
--- Assets/Scripts/GemPickup.cs
+++ Assets/Scripts/GemPickup.cs
@@ -8,16 +8,15 @@ public class GemPickup : MonoBehaviour
 		if (IsGem(hit)) CollectGem(hit); 
 	}
 
+	// Check if the hit gameobject has the Gem component on it
 	private bool IsGem(ControllerColliderHit hit) {
-		return (hit.gameObject.GetComponent<Gem>() != null);
+		return false;
 	}
 
-	// Play ding & disactivate gem
+	// Get the audio source component of the player & play ding!
+	// Disable the gem game object
+	// You can use print() to test this collision mechanism
 	private void CollectGem(ControllerColliderHit hit) {
-		GameObject gem = hit.collider.gameObject;
-		AudioSource source = gameObject.GetComponent<AudioSource>();
-		source.Play();
-		gem.SetActive(false);
-		print("Collecting gem!");
+		
 	}
 }
