cd17d7f688fa555f2e08451e601e379904d094b3
amyxlase <amyclin9@gmail.com>
2023-02-11 21:45:58 -0500

    initial push

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gem : MonoBehaviour {
+
+    public Vector3 rotationAngle;
+    public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(rotationAngle * rotationSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/GemPickup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class GemPickup : MonoBehaviour
+{
+
+	private void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+		if (IsGem(hit)) CollectGem(hit); 
+	}
+
+	private bool IsGem(ControllerColliderHit hit) {
+		return (hit.gameObject.GetComponent<Gem>() != null);
+	}
+
+	// Play ding & disactivate gem
+	private void CollectGem(ControllerColliderHit hit) {
+		GameObject gem = hit.collider.gameObject;
+		AudioSource source = gameObject.GetComponent<AudioSource>();
+		source.Play();
+		gem.SetActive(false);
+		print("Collecting gem!");
+	}
+}
